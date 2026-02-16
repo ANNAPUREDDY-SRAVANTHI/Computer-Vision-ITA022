c5cc9e554c13898ac63960166fea432ce6610ded
ANNAPUREDDY-SRAVANTHI <162045235+ANNAPUREDDY-SRAVANTHI@users.noreply.github.com>
2026-02-16 14:51:17 +0530

    Add files via upload

---
+++ 31 segmentation algorithm.py
@@ -0,0 +1,7 @@
+import cv2 
+img = cv2.imread("sample.jpg") 
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+_, thresh = cv2.threshold(gray, 120, 255, cv2.THRESH_BINARY) 
+cv2.imshow("Segmented Image", thresh)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
