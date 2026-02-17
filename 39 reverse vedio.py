7fcba00c864c41c847dd088b831ea9421bd4463b
ANNAPUREDDY-SRAVANTHI <162045235+ANNAPUREDDY-SRAVANTHI@users.noreply.github.com>
2026-02-17 09:37:38 +0530

    Add files via upload

---
+++ 39 reverse vedio.py
@@ -0,0 +1,22 @@
+import cv2
+cap = cv2.VideoCapture(r'C:\Users\asrav\OneDrive\Pictures\Desktop\python\ImageProject\sample vedio.mp4')
+frames = []
+while True:
+    ret, f = cap.read()
+    if not ret: break
+    frames.append(f)
+
+cap.release()
+
+h, w = frames[0].shape[:2]
+out = cv2.VideoWriter("reverse.mp4",
+                      cv2.VideoWriter_fourcc(*'mp4v'),
+                      30, (w, h))
+
+for f in frames[::-1]:
+    out.write(f)
+    cv2.imshow("Reverse", f)
+    if cv2.waitKey(30) == 27: break    
+
+out.release()
+cv2.destroyAllWindows()
